b924f8052b0c8913c4a1b8a6c831927c80732d4d
EduardoGomez1701 <edojose046@hotmail.com>
2025-03-27 11:21:07 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const MenuV = document.getElementById('MenuV');
+
+// Agrega un evento de clic al botón
+MenuV.addEventListener('click', () => {
+    // Redirige a la página MenuVinios.html
+    window.location.href = 'MenuVinos.html';
+});
+
+const MenuW = document.getElementById('MenuW');
+
+// Agrega un evento de clic al botón
+MenuW.addEventListener('click', () => {
+    // Redirige a la página MenuVinios.html
+    window.location.href = 'MenuWhysky.html';
+});
